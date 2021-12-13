ae16d538ece7b41a00bdfcebea12304bab2846ba
christopherzito <christopherzito11@gmail.com>
2021-12-13 18:52:44 +0100

    Update forum

--- src/components/Forum.vue
+++ src/components/Forum.vue
@@ -1,14 +1,26 @@
 <template>
-  <div id="container">
-        <h1>
-            GO TO OUR FORUM 
-        </h1>
-        <section>
-            <!-- title -->
-            <!-- text -->
-            <!-- button -->
-        </section>
-  </div>
+    <div id="container">
+            <h1>
+                GO TO OUR FORUM 
+            </h1>
+            <section>
+                <div id="box">
+                     <!-- title -->
+                    <h1>
+                        Hey! join to our
+                        Community
+                    </h1>
+                    <!-- text -->
+                    <p>
+                        Nulla molestie tortor nec lectus venenatis, sed blandit deu finibus. Curabitur feugia vulputate purus ipsums dolores ficilis uns etra.
+                    </p>
+                    <!-- button -->
+                    <button>
+                        SING UP <i class="fas fa-angle-right"></i>
+                    </button>
+                </div>
+            </section>
+    </div>
 </template>
 
 <script>
@@ -19,11 +31,42 @@ export default {
 
 <style scoped lang="scss">
 #container {
-    width: 60%;
-    margin: auto;
-    padding: 50px 0;
     text-align: center;
-    /* debug */
-    background-color: khaki;
+    background-color: black;
+    color: #ffff;
+
+    h1 {
+        padding: 50px 0;
+    }
+
+    section {
+        background-image: url("../assets/img/join_now_cta_bg.jpg");
+        width: 100%;
+        /* height: 400px; */
+
+        #box {
+            width: 30%;
+            margin: auto;
+            padding: 140px 0;
+        }
+
+        h1 {
+            font-size: 50px;
+            padding: 0;
+        }
+
+        p {
+            margin: 20px 0;
+            line-height: 40px;
+        }
+
+        button {
+            font-weight: 600;
+            color: #ffff;
+            padding: 10px 30px;
+            background-color: #2295af;
+        }
+    }
 }
+
 </style>
